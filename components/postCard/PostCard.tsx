3db488c12d0ff5565c76453db52090fa144e2076
juliomc23 <julito2302mc@gmail.com>
2022-11-08 13:07:40 +0100

    delete post and edit post functionality

--- components/postCard/PostCard.tsx
+++ components/postCard/PostCard.tsx
@@ -1,31 +1,38 @@
 import { Post } from "../../interfaces/Posts";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
+import { useDispatch } from "react-redux";
+import { deletePost as deleteRedux } from "../../redux/slices/postsSlices";
+import toast, { Toaster } from "react-hot-toast";
+import { Link } from "react-router-dom";
 
 import "./styles.css";
-import { Link } from "react-router-dom";
 
 type Props = {
   post: Post;
 };
 
-const deletePost = async (id: number) => {
-  console.log("borrar", id);
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${id.toString()}`,
-    {
-      method: "DELETE",
-    }
-  );
-
-  if (response.ok) {
-    const data = await response.json();
-
-    alert("eliminado");
-  }
-};
-
 const PostCard = ({ post }: Props) => {
+  const dispatch = useDispatch();
+
+  const deletePost = async (id: number) => {
+    console.log("borrar", id);
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${id.toString()}`,
+      {
+        method: "DELETE",
+      }
+    );
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      dispatch(deleteRedux(id));
+      toast.success("Post deleted successfully!");
+      return data;
+    }
+  };
+
   return (
     <div className="postCard__div">
       <h3 className="postCard__title">{post.title}</h3>
@@ -42,6 +49,8 @@ const PostCard = ({ post }: Props) => {
           <EditIcon sx={{ color: "black" }} />
         </Link>
       </section>
+
+      <Toaster />
     </div>
   );
 };
--- redux/slices/postsSlices.ts
+++ redux/slices/postsSlices.ts
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
 import { Post } from "../../interfaces/Posts";
+import { Router } from "react-router-dom";
 
 export type CounterState = Post[];
 
@@ -14,11 +15,26 @@ export const counterSlice = createSlice({
     getPosts: (state, action) => {
       return action.payload;
     },
-    // delete: (state) => {},
-    // edit: (state, action: PayloadAction<number>) => {},
+
+    deletePost: (state, action) => {
+      const postIdIndex = state.findIndex((post) => post.id === action.payload);
+      state.splice(postIdIndex, 1);
+    },
+
+    updatePost: (state, action) => {
+      state.map((posts) => console.log(posts.id));
+
+      const postIdIndex = state.findIndex(
+        (post) => post.id === action.payload.id
+      );
+      console.log(postIdIndex);
+
+      state[postIdIndex] = action.payload;
+      // console.log(postIdIndex);
+    },
   },
 });
 
-export const { getPosts } = counterSlice.actions;
+export const { getPosts, deletePost, updatePost } = counterSlice.actions;
 
 export default counterSlice.reducer;
